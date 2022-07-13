a6d5d6523920284cd1790743b6315db10d2e8111
Jeff Scott Brown <jeff@unityfoundation.io>
2022-07-13 13:39:32 -0500

    config driven i18n bundles

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/i18n/MessageSourceFactory.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/i18n/MessageSourceFactory.java
@@ -2,25 +2,20 @@ package io.unityfoundation.dds.permissions.manager.i18n;
 
 import io.micronaut.context.MessageSource;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Property;
 import io.micronaut.context.i18n.ResourceBundleMessageSource;
 import io.micronaut.runtime.context.CompositeMessageSource;
 import jakarta.inject.Singleton;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Factory
 class MessageSourceFactory {
+
     @Singleton
-    MessageSource createMessageSource() {
-        return new CompositeMessageSource(Arrays.asList(
-                new ResourceBundleMessageSource("i18n.applications"),
-                new ResourceBundleMessageSource("i18n.footer"),
-                new ResourceBundleMessageSource("i18n.groups"),
-                new ResourceBundleMessageSource("i18n.home"),
-                new ResourceBundleMessageSource("i18n.messages"),
-                new ResourceBundleMessageSource("i18n.navigation"),
-                new ResourceBundleMessageSource("i18n.topics"),
-                new ResourceBundleMessageSource("i18n.users")
-        ));
+    MessageSource createMessageSource(@Property(name = "i18n.bundles") List<String> ids) {
+        List<MessageSource> resourceBundleMessageSources = ids.stream().map(id -> new ResourceBundleMessageSource("i18n." + id)).collect(Collectors.toList());
+        return new CompositeMessageSource(resourceBundleMessageSources);
     }
 }
