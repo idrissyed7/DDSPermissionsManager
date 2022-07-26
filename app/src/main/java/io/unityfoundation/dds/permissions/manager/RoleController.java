a3245de82802b65e81223340a51aafa96025de9b
montesm <montesm@objectcomputing.com>
2022-07-26 16:35:10 -0500

    Add role control logic in place of duplicated user controller

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/RoleController.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/RoleController.java
@@ -1,48 +1,17 @@
 package io.unityfoundation.dds.permissions.manager;
 
-import io.micronaut.data.model.Pageable;
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
-import io.unityfoundation.dds.permissions.manager.model.user.User;
-import io.unityfoundation.dds.permissions.manager.model.user.UserRepository;
-import io.unityfoundation.dds.permissions.manager.model.user.UserService;
+import io.unityfoundation.dds.permissions.manager.model.user.Role;
 
-import javax.validation.Valid;
-import java.net.URI;
-
-@Controller("/users")
-@Secured(SecurityRule.IS_AUTHENTICATED)
-public class UserController {
-
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+@Controller("/roles")
+@Secured(SecurityRule.IS_ANONYMOUS)
+public class RoleController {
 
     @Get
-    public HttpResponse index(@Valid Pageable pageable) {
-        return HttpResponse.ok(userService.findAll(pageable));
-    }
-
-    @Get("/create")
-    public HttpResponse create() {
-        return HttpResponse.ok();
-    }
-
-    @Post("/save")
-    @Consumes(MediaType.APPLICATION_JSON)
-    HttpResponse<?> save(@Body User user) {
-        userService.save(user);
-        return HttpResponse.seeOther(URI.create("/users/"));
-    }
-
-    @Post("/delete/{id}")
-    HttpResponse<?> delete(Long id) {
-        userService.deleteById(id);
-        return HttpResponse.seeOther(URI.create("/users"));
+    public HttpResponse index() {
+        return HttpResponse.ok(Role.values());
     }
 }
