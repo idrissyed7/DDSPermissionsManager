ba38e7034bd0f4c744a1fde735b3d070b9b04f22
montesm <montesm@objectcomputing.com>
2022-07-26 16:33:59 -0500

    UFP-503 Add roles endpoint in anticipation of role selection field.

---
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/RoleController.java
@@ -0,0 +1,48 @@
+package io.unityfoundation.dds.permissions.manager;
+
+import io.micronaut.data.model.Pageable;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.*;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.unityfoundation.dds.permissions.manager.model.user.User;
+import io.unityfoundation.dds.permissions.manager.model.user.UserRepository;
+import io.unityfoundation.dds.permissions.manager.model.user.UserService;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@Controller("/users")
+@Secured(SecurityRule.IS_AUTHENTICATED)
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Get
+    public HttpResponse index(@Valid Pageable pageable) {
+        return HttpResponse.ok(userService.findAll(pageable));
+    }
+
+    @Get("/create")
+    public HttpResponse create() {
+        return HttpResponse.ok();
+    }
+
+    @Post("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    HttpResponse<?> save(@Body User user) {
+        userService.save(user);
+        return HttpResponse.seeOther(URI.create("/users/"));
+    }
+
+    @Post("/delete/{id}")
+    HttpResponse<?> delete(Long id) {
+        userService.deleteById(id);
+        return HttpResponse.seeOther(URI.create("/users"));
+    }
+}
