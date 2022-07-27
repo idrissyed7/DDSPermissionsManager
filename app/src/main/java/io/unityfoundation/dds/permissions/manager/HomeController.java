04f85971d05b3941fba00ef24558ec87c865073c
Jeff Brown <jeff@unityfoundation.io>
2022-07-27 10:52:30 -0500

    Remove model from home page.
    This was here as a workaround for an issue that prevented the authentication object from being populated in the model.  This is no longer relevant.

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/HomeController.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/HomeController.java
@@ -6,14 +6,12 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 
-import java.util.Map;
-
 @Controller
 public class HomeController {
 
     @Get
     @Secured(SecurityRule.IS_ANONYMOUS)
     public HttpResponse home() {
-        return HttpResponse.ok(Map.of("something", "Something"));
+        return HttpResponse.ok();
     }
 }
