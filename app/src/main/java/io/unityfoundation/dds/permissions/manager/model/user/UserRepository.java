682204ed15a497bfba5e023c4dfc176f126af07c
Jeff Scott Brown <jeff@unityfoundation.io>
2022-08-16 07:26:58 -0500

    Revert "UFP-533 - starting filling in filtering"
    This reverts commit 26a81ae10d7c990a0fc124960de5799442b907e4.

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserRepository.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserRepository.java
@@ -1,8 +1,6 @@
 package io.unityfoundation.dds.permissions.manager.model.user;
 
 import io.micronaut.data.annotation.Repository;
-import io.micronaut.data.model.Page;
-import io.micronaut.data.model.Pageable;
 import io.micronaut.data.repository.PageableRepository;
 
 import java.util.List;
@@ -12,5 +10,4 @@ import java.util.Optional;
 public interface UserRepository extends PageableRepository<User, Long> {
     Iterable<User> findAllByIdNotInList(List ids);
     Optional<User> findByEmail(String email);
-    Page<User> findAllByIdIn(List<Long> groupIds, Pageable pageable);
 }
--- app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserService.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserService.java
@@ -2,25 +2,20 @@ package io.unityfoundation.dds.permissions.manager.model.user;
 
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
-import io.micronaut.security.authentication.Authentication;
-import io.micronaut.security.utils.SecurityService;
-import io.unityfoundation.dds.permissions.manager.model.group.GroupRepository;
 import io.unityfoundation.dds.permissions.manager.model.groupuser.GroupUserService;
+import io.unityfoundation.dds.permissions.manager.model.group.GroupRepository;
 import jakarta.inject.Singleton;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
 
 @Singleton
 public class UserService {
-    private final SecurityService securityService;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
     private final GroupUserService groupUserService;
 
-    public UserService(SecurityService securityService, UserRepository userRepository, GroupRepository groupRepository, GroupUserService groupUserService) {
-        this.securityService = securityService;
+    public UserService(UserRepository userRepository, GroupRepository groupRepository, GroupUserService groupUserService) {
         this.userRepository = userRepository;
         this.groupRepository = groupRepository;
         this.groupUserService = groupUserService;
@@ -31,23 +26,8 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
-    /**
-     * @param pageable
-     * @return If the currently authenticated user is a super-admin then
-     * return a list of all Users for the tiven pageable.
-     * If the currently authenticated user is not a super-admin, then this
-     * will return a list of Users who are members of Groups that the
-     * currently authenticated user is also a member of, for the given
-     * pageable.
-     */
     public Page<User> findAll(Pageable pageable) {
-        if (isCurrentUserAdmin()) {
-            return userRepository.findAll(pageable);
-        } else {
-            Long userId = getCurrentlyAuthenticatedUser().getId();
-            List<Long> groupsList = groupUserService.getAllGroupsUserIsAMemberOf(userId);
-            return userRepository.findAllByIdIn(groupsList, pageable);
-        }
+        return userRepository.findAll(pageable);
     }
 
     @Transactional
@@ -74,16 +54,5 @@ public class UserService {
     @Transactional(Transactional.TxType.REQUIRES_NEW)
     public void removeUserFromGroups(Long userId) {
         groupUserService.removeUserFromAllGroups(userId);
-    }
-
-    public boolean isCurrentUserAdmin() {
-        Authentication authentication = securityService.getAuthentication().get();
-        return Optional.of((Boolean) authentication.getAttributes().get("isAdmin")).orElse(false);
-    }
-
-    public User getCurrentlyAuthenticatedUser() {
-        Authentication authentication = securityService.getAuthentication().get();
-        String userEmail = authentication.getName();
-        return getUserByEmail(userEmail).get();
     }
 }
