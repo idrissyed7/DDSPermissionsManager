26a81ae10d7c990a0fc124960de5799442b907e4
Jeff Scott Brown <jeff@unityfoundation.io>
2022-08-15 21:41:45 -0500

    UFP-533 - starting filling in filtering

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserService.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/model/user/UserService.java
@@ -2,20 +2,25 @@ package io.unityfoundation.dds.permissions.manager.model.user;
 
 import io.micronaut.data.model.Page;
 import io.micronaut.data.model.Pageable;
-import io.unityfoundation.dds.permissions.manager.model.groupuser.GroupUserService;
+import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.utils.SecurityService;
 import io.unityfoundation.dds.permissions.manager.model.group.GroupRepository;
+import io.unityfoundation.dds.permissions.manager.model.groupuser.GroupUserService;
 import jakarta.inject.Singleton;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Singleton
 public class UserService {
+    private final SecurityService securityService;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
     private final GroupUserService groupUserService;
 
-    public UserService(UserRepository userRepository, GroupRepository groupRepository, GroupUserService groupUserService) {
+    public UserService(SecurityService securityService, UserRepository userRepository, GroupRepository groupRepository, GroupUserService groupUserService) {
+        this.securityService = securityService;
         this.userRepository = userRepository;
         this.groupRepository = groupRepository;
         this.groupUserService = groupUserService;
@@ -26,8 +31,23 @@ public class UserService {
         return userRepository.findByEmail(email);
     }
 
+    /**
+     * @param pageable
+     * @return If the currently authenticated user is a super-admin then
+     * return a list of all Users for the tiven pageable.
+     * If the currently authenticated user is not a super-admin, then this
+     * will return a list of Users who are members of Groups that the
+     * currently authenticated user is also a member of, for the given
+     * pageable.
+     */
     public Page<User> findAll(Pageable pageable) {
-        return userRepository.findAll(pageable);
+        if (isCurrentUserAdmin()) {
+            return userRepository.findAll(pageable);
+        } else {
+            Long userId = getCurrentlyAuthenticatedUser().getId();
+            List<Long> groupsList = groupUserService.getAllGroupsUserIsAMemberOf(userId);
+            return userRepository.findAllByIdIn(groupsList, pageable);
+        }
     }
 
     @Transactional
@@ -54,5 +74,16 @@ public class UserService {
     @Transactional(Transactional.TxType.REQUIRES_NEW)
     public void removeUserFromGroups(Long userId) {
         groupUserService.removeUserFromAllGroups(userId);
+    }
+
+    public boolean isCurrentUserAdmin() {
+        Authentication authentication = securityService.getAuthentication().get();
+        return Optional.of((Boolean) authentication.getAttributes().get("isAdmin")).orElse(false);
+    }
+
+    public User getCurrentlyAuthenticatedUser() {
+        Authentication authentication = securityService.getAuthentication().get();
+        String userEmail = authentication.getName();
+        return getUserByEmail(userEmail).get();
     }
 }
