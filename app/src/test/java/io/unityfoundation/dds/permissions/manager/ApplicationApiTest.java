eeda02124f3010f57f6f545607038273b56aa015
Jeff Scott Brown <jeff@unityfoundation.io>
2022-09-28 17:27:09 -0500

    UFP-703 - More data validation

--- app/src/test/java/io/unityfoundation/dds/permissions/manager/ApplicationApiTest.java
+++ app/src/test/java/io/unityfoundation/dds/permissions/manager/ApplicationApiTest.java
@@ -864,24 +864,42 @@ public class ApplicationApiTest {
     @Test
     public void testApplicationFilter() {
         HttpRequest<Object> request = HttpRequest.GET("/applications/search?filter=Group");
-        List results = blockingClient.retrieve(request, List.class);
-        assertEquals(10, results.size());
+        List<Map> results = blockingClient.retrieve(request, List.class);
+
+        List<String> expectedApplicationNames = Arrays.asList("Go", "Groovy", "Micronaut", "Guitar", "Piano", "Drums", "Paint", "Clay", "Pencil", "Group Psychology");
+        assertResultContainsAllExpectedApplicationNames(expectedApplicationNames, results);
 
         request = HttpRequest.GET("/applications/search?filter=Group&size=7");
         results = blockingClient.retrieve(request, List.class);
         assertEquals(7, results.size());
+        assertResultOnlyContainsExpectedApplicationNames(results, expectedApplicationNames);
 
         request = HttpRequest.GET("/applications/search?filter=Art");
         results = blockingClient.retrieve(request, List.class);
-        assertEquals(4, results.size());
+
+        expectedApplicationNames = Arrays.asList("Paint", "Clay", "Pencil", "Art");
+        assertResultContainsAllExpectedApplicationNames(expectedApplicationNames, results);
 
         request = HttpRequest.GET("/applications/search?filter=Art&size=2");
         results = blockingClient.retrieve(request, List.class);
         assertEquals(2, results.size());
+        assertResultOnlyContainsExpectedApplicationNames(results, expectedApplicationNames);
 
         request = HttpRequest.GET("/applications/search?filter=Clay");
         results = blockingClient.retrieve(request, List.class);
         assertEquals(1, results.size());
+        assertEquals("Clay", results.get(0).get("name"));
+    }
+
+    private void assertResultOnlyContainsExpectedApplicationNames(List<Map> results, List<String> expectedApplicationNames) {
+        assertTrue(results.stream().allMatch((m) -> expectedApplicationNames.contains(m.get("name"))));
+    }
+
+    private void assertResultContainsAllExpectedApplicationNames(List<String> expectedApplicationNames, List<Map> results) {
+        List<String> applicationNames = results.stream().map(m -> (String)m.get("name")).collect(Collectors.toList());
+
+        assertEquals(expectedApplicationNames.size(), applicationNames.size());
+        assertTrue(expectedApplicationNames.containsAll(applicationNames));
     }
 
     private HttpResponse<?> createGroup(String groupName) {
