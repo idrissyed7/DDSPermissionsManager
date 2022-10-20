53c48169af00b78e5ce865e7e27e48429d9480b0
montesm <montesm@objectcomputing.com>
2022-10-20 12:00:18 -0500

    UFP-780 Add database size constraint for Group, Application, and Topic Names

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/model/topic/Topic.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/model/topic/Topic.java
@@ -6,6 +6,7 @@ import io.unityfoundation.dds.permissions.manager.model.group.Group;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "permissions_topic")
@@ -17,6 +18,7 @@ public class Topic {
 
     @NonNull
     @NotBlank
+    @Size(min = 3)
     private String name;
 
     @NonNull
