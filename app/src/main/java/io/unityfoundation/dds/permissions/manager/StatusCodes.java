12eb3ac193e24070b06b31c03e4730a12696d49a
montesm <montesm@objectcomputing.com>
2022-10-26 16:17:08 -0500

    Use interface and strings instead.

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/StatusCodes.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/StatusCodes.java
@@ -1,40 +1,40 @@
 package io.unityfoundation.dds.permissions.manager;
 
-public enum StatusCodes {
+public interface StatusCodes {
     // generic
-    UNAUTHORIZED,
+    String UNAUTHORIZED = "unauthorized";
 
     // user
-    USER_ALREADY_EXISTS,
-    USER_NOT_FOUND,
+    String USER_ALREADY_EXISTS = "user.exists";
+    String USER_NOT_FOUND = "user.not-found";
 
     // group
-    GROUP_NOT_FOUND,
-    GROUP_ALREADY_EXISTS,
-    GROUP_NAME_CANNOT_BE_BLANK_OR_NULL,
+    String GROUP_NOT_FOUND = "group.not-found";
+    String GROUP_ALREADY_EXISTS = "group.exists";
+    String GROUP_NAME_CANNOT_BE_BLANK_OR_NULL = "group.name.cannot-be-blank-or-null";
 
     // application
-    APPLICATION_NOT_FOUND,
-    APPLICATION_REQUIRES_GROUP_ASSOCIATION,
-    APPLICATION_CANNOT_UPDATE_GROUP_ASSOCIATION,
-    APPLICATION_ALREADY_EXISTS,
-    APPLICATION_NAME_CANNOT_BE_BLANK_OR_NULL,
-    IDENTITY_CERT_NOT_FOUND,
-    PERMISSIONS_CERT_NOT_FOUND,
-    GOVERNANCE_FILE_NOT_FOUND,
+    String APPLICATION_NOT_FOUND = "application.not-found";
+    String APPLICATION_REQUIRES_GROUP_ASSOCIATION = "application.requires-group-association";
+    String APPLICATION_CANNOT_UPDATE_GROUP_ASSOCIATION = "application.cannot-update-group-association";
+    String APPLICATION_ALREADY_EXISTS = "application.exists";
+    String APPLICATION_NAME_CANNOT_BE_BLANK_OR_NULL = "application.name.cannot-be-blank-or-null";
+    String IDENTITY_CERT_NOT_FOUND = "application.identity-ca-cert.not-found";
+    String PERMISSIONS_CERT_NOT_FOUND = "application.permissions-ca-cert.not-found";
+    String GOVERNANCE_FILE_NOT_FOUND = "application.governance-file.not-found";
 
     // topic
-    TOPIC_NOT_FOUND,
-    TOPIC_UPDATE_NOT_ALLOWED,
-    TOPIC_REQUIRES_GROUP_ASSOCIATION,
-    TOPIC_NAME_CANNOT_BE_BLANK_OR_NULL,
-    TOPIC_ALREADY_EXISTS,
+    String TOPIC_NOT_FOUND = "topic.not-found";
+    String TOPIC_UPDATE_NOT_ALLOWED = "topic.update-not-allowed";
+    String TOPIC_REQUIRES_GROUP_ASSOCIATION = "topic.requires-group-association";
+    String TOPIC_NAME_CANNOT_BE_BLANK_OR_NULL = "topic.name.cannot-be-blank-or-null";
+    String TOPIC_ALREADY_EXISTS = "topic.exists";
 
     // group member
-    USER_IS_VALID,
-    USER_IS_NOT_VALID,
-    GROUP_MEMBER_NOT_FOUND,
+    String USER_IS_VALID = "user.is-valid";
+    String USER_IS_NOT_VALID = "user.is-not-valid";
+    String GROUP_MEMBERSHIP_NOT_FOUND = "user.group-membership.not-found";
 
     // application permission
-    APPLICATION_PERMISSION_NOT_FOUND
+    String APPLICATION_PERMISSION_NOT_FOUND = "application.permission.not-found";
 }
