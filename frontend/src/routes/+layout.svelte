643f2eb3fe4fd601c6d8f1ae1740ca050e03c598
Daniel Bellone <belloned@objectcomputing.com>
2022-12-02 06:50:57 -0800

    UFP-837 401 Logged To Console On Load

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -16,11 +16,12 @@
 
 	onMount(async () => {
 		try {
-			const res = await httpAdapter.get(`/token_info`);
-
-			onLoggedIn(res.data);
-
-			avatarName = res.data.username.slice(0, 1).toUpperCase();
+			const userLoggedCookie = document.cookie;
+			if (userLoggedCookie.includes('JWT_REFRESH_TOKEN')) {
+				const res = await httpAdapter.get(`/token_info`);
+				onLoggedIn(res.data);
+				avatarName = res.data.username.slice(0, 1).toUpperCase();
+			}
 
 			console.log('is authenticated?', $isAuthenticated);
 			console.log('is Admin? ', $isAdmin);
