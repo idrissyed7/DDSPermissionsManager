92366cf9b6fa80234deddf7b5926ebde0ffc0373
montesm <montesm@objectcomputing.com>
2022-12-19 15:09:29 -0600

    UFP-928 Add test email address in database for authorization

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/security/AuthenticationProviderUserPassword.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/security/AuthenticationProviderUserPassword.java
@@ -36,7 +36,12 @@ public class AuthenticationProviderUserPassword implements AuthenticationProvide
         if ( (identity.equals("unity") || identity.equals("unity-admin")) && password.equals("password") ) {
             if (environment.getActiveNames().contains("dev") || environment.getActiveNames().contains("test")) {
 
-                return Publishers.just(AuthenticationResponse.success((String) authenticationRequest.getIdentity(),
+                String testEmail = "unity-test@etest.test";
+                if ( identity.equals("unity-admin") ){
+                    testEmail = "unity-admin-test@test.test";
+                }
+
+                return Publishers.just(AuthenticationResponse.success( testEmail,
                         (identity.equals("unity-admin") ? List.of(UserRole.ADMIN.toString()) : Collections.emptyList())
                 ));
             }
