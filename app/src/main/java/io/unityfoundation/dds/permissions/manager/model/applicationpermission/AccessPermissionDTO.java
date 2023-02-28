2974a83f03e66539d464af3fc2b930bc1add6e02
montesm <montesm@objectcomputing.com>
2023-02-28 13:56:09 -0600

    UFP-994 Include Topic Group Name in ApplicationPermission response

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/model/applicationpermission/AccessPermissionDTO.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/model/applicationpermission/AccessPermissionDTO.java
@@ -6,16 +6,18 @@ import io.micronaut.core.annotation.Introspected;
 public class AccessPermissionDTO {
     private final Long topicId;
     private final String topicName;
+    private final String topicGroup;
     private final Long applicationId;
     private final String applicationName;
     private final String applicationGroupName;
     private final AccessType accessType;
     private final Long id;
 
-    public AccessPermissionDTO(Long id, Long topicId, String topicName, Long applicationId, String applicationName, String applicationGroupName, AccessType accessType) {
+    public AccessPermissionDTO(Long id, Long topicId, String topicName, String topicGroup, Long applicationId, String applicationName, String applicationGroupName, AccessType accessType) {
         this.id = id;
         this.topicId = topicId;
         this.topicName = topicName;
+        this.topicGroup = topicGroup;
         this.applicationId = applicationId;
         this.applicationName = applicationName;
         this.applicationGroupName = applicationGroupName;
@@ -48,5 +50,9 @@ public class AccessPermissionDTO {
 
     public String getApplicationGroupName() {
         return applicationGroupName;
+    }
+
+    public String getTopicGroup() {
+        return topicGroup;
     }
 }
