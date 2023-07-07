126a461002c455b0596cfe00ff4c4f59634897bd
Son Dinh <dinhs@objectcomputing.com>
2023-07-07 14:58:59 -0500

    From review

--- app/src/main/java/io/unityfoundation/dds/permissions/manager/util/XMLEscaper.java
+++ app/src/main/java/io/unityfoundation/dds/permissions/manager/util/XMLEscaper.java
@@ -2,35 +2,21 @@ package io.unityfoundation.dds.permissions.manager.util;
 
 import jakarta.inject.Singleton;
 
-import java.util.HashMap;
-
 @Singleton
 public class XMLEscaper {
-    private HashMap<String, String> cache = new HashMap<String, String>();
-
     public String escape(String input) {
-        if (cache.containsKey(input)) {
-            return cache.get(input);
-        }
         // Five characters in XML text that need escaping: <, >, ', ", &
-        String result = input.replace("<", "\\<")
+        return input.replace("<", "\\<")
                 .replace(">", "\\>")
                 .replace("'", "\\'")
                 .replace("\"", "\\\"")
                 .replace("&", "\\&");
-        cache.put(input, result);
-        return result;
     }
 
     // Names resulting from X500NameBuilder already have characters
     // '<', '>', and '"' escaped. Only to escape ''' and '&' here.
     public String escapeX500Name(String input) {
-        if (cache.containsKey(input)) {
-            return cache.get(input);
-        }
-        String result = input.replace("'", "\\'")
+        return input.replace("'", "\\'")
                 .replace("&", "\\&");
-        cache.put(input, result);
-        return result;
     }
 }
